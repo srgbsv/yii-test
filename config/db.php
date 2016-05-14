99d3c30568ec1aa95954bb4d9e3cf23193143e0d
Sergey Bosov <srg.bsv@gmail.com>
2016-05-14 12:43:02 +0300

    Init commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=test',
+    'username' => 'root',
+    'password' => '1234567',
+    'charset' => 'utf8',
+];
---
+++ migrations/m160513_201923_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `user_table`.
+ */
+class m160513_201923_create_user_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'name' => Schema::TYPE_STRING . 'NOT NULL',
+            'password_hash' => Schema::TYPE_SMALLINT . 'NOT NULL',
+            'email' => Schema::TYPE_STRING . 'NOT NULL',
+            'role' => Schema::TYPE_INTEGER . 'NOT NULL',
+            'status' => Schema::TYPE_INTEGER . 'NOT NULL',
+            'created_at' => Schema::TYPE_INTEGER . 'NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . 'NOT NULL'
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('user_table');
+    }
+}
